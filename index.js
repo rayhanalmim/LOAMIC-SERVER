4f20e66721b02a85d55714ed93af44f8855265c2
rayhanalmim <rayhanalmim1@gmail.com>
2023-12-23 15:02:43 +0600

    setup loamic server and deploy it on vercel

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://loamicDB:02hYw9qqhERUyT41@cluster0.tdvw5wt.mongodb.net/loamicDB?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error(`Error connecting to MongoDB: ${err}`);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+
+  const employeeCollection = mongoose.model('task', new mongoose.Schema({}, { strict: false }));
+
+});
+
+app.get('/', (req, res) => {
+  res.send('Loamic server running');
+});
+
+app.listen(port, () => {
+  console.log(`Loamic Server is running on port ${port}`);
+});
