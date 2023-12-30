8ea71ef752b7073f39dcb2c8a36a7e4d21ba2aa6
rayhanalmim <rayhanalmim1@gmail.com>
2023-12-30 14:28:04 +0600

    added company and admin information in database

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(express.json())
 const uri = `mongodb+srv://loamicDB:02hYw9qqhERUyT41@cluster0.tdvw5wt.mongodb.net/loamicDB?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -23,28 +21,56 @@ db.on('error', (err) => {
 db.once('open', () => {
   console.log('Connected to MongoDB');
 
-  const userCollection = mongoose.model('users', new mongoose.Schema({}, { strict: false }));
-  const contractCollection = mongoose.model('contract', new mongoose.Schema({}, { strict: false }));
-
-  app.get('/users', async(req, res)=>{
-    const result = await userCollection.find();
-    res.send(result);
-  })
-
-  app.get('/currentUser', async (req, res)=>{
-    const pin = req.query.pin;
-    const pinInt = parseInt(pin);
-    console.log(pinInt)
-    const result = await userCollection.find({ Pin : pinInt})
-    res.send(result);
-  })
-
-  app.get('/contract', async (req, res)=>{
-    const result = await contractCollection.find();
-    res.send(result);
-  })
-
 });
+
+const userCollection = mongoose.model('users', new mongoose.Schema({}, { strict: false }));
+const contractCollection = mongoose.model('contract', new mongoose.Schema({}, { strict: false }));
+const projectCollection = mongoose.model('project', new mongoose.Schema({}, { strict: false }));
+const companyInfo = mongoose.model('aboutUs', new mongoose.Schema({}, { strict: false }));
+const adminCollection = mongoose.model('UserRole', new mongoose.Schema({}, { strict: false }));
+
+app.get('/users', async(req, res)=>{
+  const result = await userCollection.find();
+  res.send(result);
+})
+
+app.get('/aboutUs', async(req, res)=>{
+  const result = await companyInfo.find();
+  res.send(result);
+})
+
+app.get('/currentUser', async (req, res)=>{
+  const pin = req.query.pin;
+  const pinInt = parseInt(pin);
+  console.log(pinInt)
+  const result = await userCollection.find({ Pin : pinInt})
+  res.send(result);
+})
+
+app.get('/contract', async (req, res)=>{
+  const result = await contractCollection.find();
+  res.send(result);
+})
+
+app.get('/projects', async (req, res)=>{
+  const result = await projectCollection.find();
+  res.send(result);
+})
+
+// ------------------------------checkAdmin-------------------------------------
+app.get('/isAdmin', async (req, res)=>{
+  const email = req.query.email;
+  const password = req.query.password;
+  console.log(email, password)
+  const isExists = await adminCollection.findOne({ email_address: email, password: password })
+  console.log(isExists)
+  if(isExists){
+    return res.send(isExists)
+  }
+  else{
+    return res.status(402).send({message: 'Unauthorize access'})
+  }
+})
 
 app.get('/', (req, res) => {
   res.send('Loamic server running');
